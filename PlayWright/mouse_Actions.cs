1973b87607a32b960ab3d9a457585a6104ba6dc2
sreenanda kumar <sreen@SREENANDAKUMAR>
2026-07-22 10:54:13 +0530

    Add MOUSEACTIONS() test for Playwright mouse events
    Added MOUSEACTIONS() to mouse_Actions class to demonstrate various mouse interactions using Microsoft.Playwright. Actions include filling inputs, double/right/left clicking, hovering, drag-and-drop, mouse movements, mouse down/up, and scrolling. Includes waits for action observation.

--- PlayWright/mouse_Actions.cs
+++ PlayWright/mouse_Actions.cs
@@ -1,13 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Playwright;
 
 namespace playwright
 {
-    public  class mouse_Actions
+    public class mouse_Actions
     {
 
 
 
+       
+
+            [Test]
+
+            public async Task MOUSEACTIONS()
+            {
+
+                using var nanda = await Playwright.CreateAsync();
+                await using var nanda1 = await nanda.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+                {
+                    Headless = false,
+                });
+
+                var page = await nanda1.NewPageAsync();
+
+
+
+                await page.GotoAsync("https://testautomationpractice.blogspot.com/");
+
+            await page.Locator("//input[@id=\"name\"]").FillAsync("nanda kumar");
+
+            await page.Locator("//input[@id=\"name\"]").DblClickAsync();
+
+            await page.Locator("//input[@id=\"email\"]").ClickAsync(new LocatorClickOptions { Button = MouseButton.Right });
+            await page.WaitForTimeoutAsync(3000);
+
+            await page.Locator("//input[@id=\"phone\"]").ClickAsync(new LocatorClickOptions { Button = MouseButton.Left });
+
+            await page.WaitForTimeoutAsync(3000);
+
+
+            await page.Locator("//div[@class=\"date-picker-box\"]").HoverAsync();
+
+
+            await page.Locator("//div[@id=\"draggable\"]").DragToAsync(page.Locator("//div[@id=\"droppable\"]"));
+
+            await page.WaitForTimeoutAsync(3000);
+
+
+            await page.Mouse.MoveAsync(300, 300);
+            await page.WaitForTimeoutAsync(3000);
+
+
+
+            // Mouse Down and UP
+
+
+            await page.Mouse.DownAsync();
+            await page.WaitForTimeoutAsync(3000);
+            await page.Mouse.MoveAsync(100, 100);
+            await page.WaitForTimeoutAsync(3000);
+            await page.Mouse.UpAsync();
+            await page.WaitForTimeoutAsync(3000);
+            await page.Mouse.MoveAsync(300, 300);
+
+
+
+
+
+            await page.Mouse.WheelAsync(100, 1000);
+
+        }
+
+
+
+
+
     }
-}
+    }
