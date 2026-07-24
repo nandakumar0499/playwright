1a1182e53340a20fda04636fd9e94ceb8fd471dc
sreenanda kumar <sreen@SREENANDAKUMAR>
2026-07-24 15:06:10 +0530

    Add KeyBord_Actions class with Playwright test method
    Added KeyBord_Actions class in the playwright namespace. Implemented async KEYBORD_ACTIONS test method to initialize Playwright, launch Chromium in non-headless mode, open a new page, and navigate to a specified URL. Included necessary using directives.

---
+++ PlayWright/KeyBord Actions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Playwright;
+
+namespace playwright
+{
+    internal class KeyBord_Actions
+    {
+
+
+
+        [Test]
+
+        public async Task KEYBORD_ACTIONS()
+        {
+            using var nanda = await Playwright.CreateAsync();
+            await using var nanda1 = await nanda.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+            });
+
+            var page = await nanda1.NewPageAsync();
+
+
+
+            await page.GotoAsync("https://welcometotesting7.blogspot.com/2024/01/Seleniumjava.html");
+
+
+
+
+        }
+    }
+}
